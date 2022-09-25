e5daac13c3c228b2423d2002e8711a4d86d7982f
sohail70 <sohail.e.nia@gmail.com>
2022-09-25 23:57:26 +0330

    review

--- src/00_test.cpp
+++ src/00_test.cpp
@@ -1,24 +1,10 @@
 #include<iostream>
-#include<string.h>
-
-const char* min(const char* s , const char* t)
-{
-    return(strcmp(s,t)<0) ? s:t;
-}
-
-float min(float x, float y )
-{
-    return(x<y) ? x:y;
-}
-
-
+int addFunc(int a , int b){return a+b;}
 int main()
 {
-    const char* s = min("abc","xyz");
-    float f = min(4.4f , 1.23f);
-    int f2 = min(2011,2014);
-
-    std::cout<<s<<"\n";
-    std::cout<<f<<"\n";
-    std::cout<<f2<<"\n";
+    struct addObject{
+        int operator() (int a, int b){return a+b;};
+    };
+    addObject addObj;
+    std::cout<<addObj(2,3)<<"\n";
 }
