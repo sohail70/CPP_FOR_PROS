0f21cdcaea3a23acdf17e666a6e6042347bb1542
sohail70 <sohail.e.nia@gmail.com>
2022-10-02 20:01:21 +0330

    reveiw

--- src/00_test.cpp
+++ src/00_test.cpp
@@ -1,44 +1,24 @@
+#include<chrono>
 #include<iostream>
-
-class Account{
-    public:
-        Account(double amt): amount(amt){}
-
-        double getBalance() const{
-            return amount;
-        }
-
-        private:
-            double amount;
-};
-
-
-class BankAccount: public Account{
-    public:
-        BankAccount(double amt): Account(amt) {}
-};  
-
-class WireAccount: public Account{
-    public:
-        WireAccount(double amt): Account(amt) {}
-};
-
-class CheckingAccount: public BankAccount, public WireAccount //This class has access to the getBalance methods of both classes.
-{
-    public:
-        CheckingAccount(double amt): BankAccount(amt), WireAccount(amt){} //! Khodam: alan har kodum az az ina ke initilize beshe , har kodom ye bar Account(amt) ro ejra mikunan ke in baes mishe do bar in kar anjam begire
-};
+#include<memory>
+static const long long numInt = 100000000;
 
 int main()
 {
-    CheckingAccount checkingAccount(100.0);
-    //checkingAccount.getBalance(); //!ERROR---> ambiguous function
-    /*
-    If we try to call the getBalance method using the instance of
-    CheckingAccount class, it will give us an error, but if we call it with the
-    name of the base class along with the scope operator :: then it works
-    fine.
-    */
-    std::cout<<checkingAccount.BankAccount::getBalance()<<"\n";
-    std::cout<<checkingAccount.WireAccount::getBalance()<<"\n";
+    auto start = std::chrono::system_clock::now();
+
+    for (long long i = 0; i <numInt ; ++i)
+    {
+        //int* tmp(new int(i));
+        //delete tmp;
+        //std::shared_ptr<int> tmp(new int(i));
+       //std::shared_ptr<int> tmp(std::make_shared<int>(i));
+        //std::unique_ptr<int> tmp(new int(i));
+        std::unique_ptr<int> tmp(std::make_unique<int>(i));
+    }
+
+    std::chrono::duration<double> dur = std::chrono::system_clock::now()-start;
+    std::cout<<"time native: "<< dur.count()<<" seconds"<<"\n";
+
+
 }
