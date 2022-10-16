a5bc97044ce125ce9ca8f6a12db2523eaf4c76db
sohail70 <sohail.e.nia@gmail.com>
2022-10-16 17:28:22 +0330

    Multi-Threading

--- src/101_Exercise.cpp
+++
@@ -1,6 +0,0 @@
-/* PDF 101
-Exercise #
-Which initialization order of the attributes of a class is guaranteed?
-The order of their declaration or the invocation order in the constructor.
-Discuss the consequences of each possibility.
-*/
